62aaaf19bd0af7a76e324eb0879db10cff10600e
Akshat Kothavade <akshat123436@gmail.com>
2023-01-19 12:34:55 +0530

    added scroll and anchor tag to nav bar

--- frontend/src/components/layout/Navigation.js
+++ frontend/src/components/layout/Navigation.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Navigation.module.css";
 import { FaBars } from "react-icons/fa";
+
 function Navigation() {
   const [isVisible, setIsVisible] = useState(false);
   const clickHandler = () => {
@@ -11,11 +12,21 @@ function Navigation() {
   return (
     <nav className={`${isVisible ? styles.visible : ""}`}>
       <ul className={`${styles.nav} ${isVisible ? styles.visible : ""}`}>
-        <li className={styles["nav-item"]}>People Reviews</li>
-        <li className={styles["nav-item"]}>About Us</li>
-        <li className={styles["nav-item"]}>Terms & Conditions</li>
-        <li className={styles["nav-item"]}>Login</li>
-        <li className={styles["nav-item"]}>Register</li>
+        <li className={styles["nav-item"]}>
+          <a href="#reviews-container"> People Reviews</a>
+        </li>
+        <li className={styles["nav-item"]}>
+          <a href="#about-container">About Us</a>
+        </li>
+        <li className={styles["nav-item"]}>
+          <a href="#terms-container">Terms & Conditions</a>
+        </li>
+        <li className={styles["nav-item"]}>
+          <a href="#login-container">Login</a>
+        </li>
+        <li className={styles["nav-item"]}>
+          <a href="#register-container">Register</a>
+        </li>
       </ul>
       <FaBars
         className={`${styles.bars} ${isVisible ? styles.rotate : ""}`}

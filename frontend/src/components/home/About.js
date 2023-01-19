8e4641cc0971b7f22c9faa409b07fb137944a330
Akshat Kothavade <akshat123436@gmail.com>
2023-01-19 13:23:12 +0530

    before force install

---
+++ frontend/src/components/home/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../UI/Container";
+
+function About() {
+  return (
+    <Container>
+      <h2>ABOUT US</h2>
+    </Container>
+  );
+}
+
+export default About;
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import Navigation from "../layout/Navigation.js";
 import Reviews from "./Reviews.js";
+import About from "./About.js";
 function Home() {
   return (
     <Fragment>
       <Navigation></Navigation>
+      <About></About>
       <Reviews></Reviews>
     </Fragment>
   );
